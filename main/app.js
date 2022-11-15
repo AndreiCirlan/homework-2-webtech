da1a686e02a8710384255e1d03498d58d02c0a78
Andrei Cirlan <90861082+AndreiCirlan@users.noreply.github.com>
2022-11-15 15:43:36 +0200

    Update app.js

--- main/app.js
+++ main/app.js
@@ -1,7 +1,31 @@
-function addTokens(input, tokens){
-    
-}
+function addTokens(input, tokens) {
+    if (typeof input === 'string') {
+        if (input.length > 5) {
+            toReplace = tokens.find(token => (typeof token.tokenName !== 'string') && (token.tokenName !== null))
+            if (toReplace === undefined) {
+                if (input.includes('...') === true) {
+                    output = tokens.find(token => (typeof token.tokenName === 'string'));
+                    toReplace = input.replace('...','');
+                    finalString = toReplace.concat('${'+output.tokenName+'}');
+                    return finalString;
+                }
+                else {
+                    return input;
+                }
+            }
+            else {
+                throw new Error('Invalid array format');
+            }
+        }
+        else {
+            throw new Error('Input should have at least 6 characters');
+        }
 
+    }
+    else {
+        throw new Error('Invalid input');
+    }
+}
 const app = {
     addTokens: addTokens
 }
